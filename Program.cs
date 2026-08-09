7d1cc0e38d1c9c5075131528f1cb74cb5a7ab096
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-08-09 13:44:54 -0400

    feat: add routes and test with Scalar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using EmployeeAPI;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var employees = new List<Employee>
+{
+    new Employee { Id = 1, FirstName = "John", LastName = "Doe" },
+    new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" }
+};
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -10,8 +18,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/employees", () => Results.Ok(employees));
+
+app.MapGet("/employees/{id:int}", (int id) =>
+{
+    var employee = employees.SingleOrDefault(e => e.Id == id);
+    if (employee == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(employee);
+});
 
 app.Run();
