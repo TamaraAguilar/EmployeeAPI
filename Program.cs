4e7c409cc6524994abc2afa7159946f60a56c67a
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-08-11 22:39:00 -0400

    feat: add AddProblemDetails

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSingleton<IRepository<Employee>, EmployeeRepository>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 var employeeRoute = app.MapGroup("employees");
